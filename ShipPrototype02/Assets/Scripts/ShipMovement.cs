c2ff65f99d290a6b1352dd22d171eac0a5bbfea6
Chris <christopher.berry2018@gmail.com>
2019-10-28 20:42:35 -0600

    Turned to top down.
    Removed vertical movement.

--- ShipPrototype02/Assets/Scripts/ShipMovement.cs
+++ ShipPrototype02/Assets/Scripts/ShipMovement.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float turnSpeed;
-    public float verticalInput;
+
     public float horizontalInput;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -19,18 +18,15 @@ public class ShipMovement : MonoBehaviour
     void Update()
     {
         
-       //Vertical Input
-       verticalInput = Input.GetAxis("Vertical");
+      
        //HorizontalInput
        horizontalInput = Input.GetAxis("Horizontal");
+       transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
 
        //Move plane forward at constant speed
        transform.Translate(Vector3.left*Time.deltaTime*speed);
 
-       //Tile Plane up and down
-       transform.Rotate(-Vector3.forward, turnSpeed * verticalInput * Time.deltaTime);
-       //Tilts left and right
-       transform.Rotate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+      
 
        if (Input.GetKeyDown(KeyCode.Space)){
            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
