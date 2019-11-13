dd10e3a6ffc96cc420933d264cef84701c2b4a52
Chris <christopher.berry2018@gmail.com>
2019-11-13 16:29:45 -0700

    Changed Background. Switched prefab missiles

---
+++ ShipPrototype02/Assets/Scripts/RepeatGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatGround : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (-transform.position.x < startPos.x - repeatWidth){transform.position = startPos;}
+    }
+}
